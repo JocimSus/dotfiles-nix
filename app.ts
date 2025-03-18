bb37c92e281edd3d3f0587886820749472a59c69
Jocim <joe.susatiyo@gmail.com>
2025-03-18 15:27:50 +0700

    it is fire now

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import Network from "gi://AstalNetwork"
 import { Variable, bind } from "astal"
 
-const network = Network.get_default().get_primary()
+const network = Network
 const varThing = Variable("1")
 
-print(varThing.get())
+print(network.get_default().get_primary())
--- config/ags/widgets/components/StatusIcons.tsx
+++ config/ags/widgets/components/StatusIcons.tsx
@@ -4,10 +4,11 @@ import { Variable, bind } from "astal"
 
 const network = Network
 
-function SimpleNetworkIndicator() {
+function SimpleNetworkIndicator({ name }: { name: string }) {
     const wifi = Network.get_default().wifi
 
     return <icon
+        name={name}
         className="icon"
         tooltipText={bind(wifi, "ssid").as(String)}
         icon={bind(wifi, "iconName")
@@ -15,7 +16,9 @@ function SimpleNetworkIndicator() {
 }
 
 function NetworkIndicator() {
-    const visibleChild = Variable("2")
+    const visibleChild = Variable("0")
+    const primaryNetwork = network.get_default().get_primary()
+    visibleChild.set(`${primaryNetwork}`)
 
     return <stack
         transitionType={Gtk.RevealerTransitionType.SLIDE_UP_DOWN}
@@ -23,17 +26,14 @@ function NetworkIndicator() {
         // TODO: COME HERE AND COMPLETE THIS
         // PROBLEM: THE FUJNCTION INSIDE SETUP IS NOT CALLED AT ALL
         // visibleChild IS NOT SET INSIDE IT
-        setup={(self) => self.hook(network, visibleChild => {
-            const primaryNetwork = network.get_default().get_primary()
-            if ([1, 2].includes(primaryNetwork))
-                visibleChild.set("2")
-            else
-                visibleChild.set("2")
-        })}
+        // setup={(self) => self.hook(network, visibleChild => {
+        //     const primaryNetwork = network.get_default().get_primary()
+        //     visibleChild.set(`${primaryNetwork}`)
+        // })}
         visibleChildName={visibleChild.get()}
         >
             <label name="1" label="1" />
-            <label name="2" label="2" />
+            <SimpleNetworkIndicator name="2" />
     </stack>
 }
 
