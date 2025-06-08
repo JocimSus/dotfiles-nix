c4e7255dc4cef1a4b7d4eef0de1564c750a6ab67
Jocim <joe.susatiyo@gmail.com>
2025-06-08 22:33:59 +0700

    setting up for server

---
+++ configuration.nix
@@ -0,0 +1,54 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./common.nix
+    ];
+
+  ## System ##
+
+  ## Graphics ##
+
+  ## Desktop ##
+ 
+  ## User account ##
+  networking.hostName = "woof";
+  users.users.jocim-server = {
+    isNormalUser = true;
+    description = "jocim-server";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  ## System programs ##
+  environment.systemPackages = with pkgs; [
+    ## Low Level ##
+
+    ## Graphics ##
+
+    ## Filesystem ##
+    ntfs3g
+    usbutils
+
+    ## Terminal  ##
+    unrar
+    unzip
+    p7zip
+
+    btop-cuda
+    lunarvim
+
+    git
+    gnumake
+    cargo
+    nodejs_24
+    ripgrep
+    lazygit
+  ];
+
+}
