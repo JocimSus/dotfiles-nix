be2514d5bbd6491dffb0d023973ecad481935c5d
jocim-server <>
2025-07-11 14:34:31 +0700

    pushed forward

---
+++ services/mailserver.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  ...
+}: {
+  sops.secrets."mailserverFile" = {};
+
+  mailserver = {
+    enable = true;
+    fqdn = "mail.224668.xyz";
+    domains = [ "224668.xyz" ];
+
+    # A list of all login accounts. To create the password hashes, use
+    # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+    loginAccounts = {
+      "jocim@224668.xyz" = {
+        hashedPasswordFile = config.sops.secrets."mailserverFile".path;
+        aliases = ["postmaster@224668.xyz"];
+      };
+    };
+  };
+
+}
