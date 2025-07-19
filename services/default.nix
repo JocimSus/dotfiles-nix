063862458d0574a176f2b23aa99951995063c2ef
jocim-server <>
2025-07-19 14:26:03 +0700

    services: disabled dnsmasq

--- services/default.nix
+++ services/default.nix
@@ -4,7 +4,6 @@
     ./cloudflared.nix
     ./openssh.nix
     ./tailscale.nix
-    ./dnsmasq.nix
 
     # Fun
     # ./nginx.nix
--- services/dnsmasq.nix
+++ services/dnsmasq.nix
@@ -2,9 +2,19 @@
   services.dnsmasq = {
     enable = true;
     settings = {
-      interface = "enp3s0"; 
+      interface = [ "enp3s0" ]; 
       bind-interfaces = true;
+      except-interface = [ "lo" ];
+      listen-address = [ "192.168.1.9" ];
       address = [ "/cloud.local/192.168.1.9" ];
+      no-hosts = true;
     };
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."cloud.local".extraConfig = ''
+      reverse_proxy 192.168.1.9:8000
+    '';
   };
 }
