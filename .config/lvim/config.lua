e00a8c4f7d025ab1b5f56ad115b9784c95ebf5e1
Jocim <joe.susatiyo@gmail.com>
2025-07-21 22:16:34 +0700

    .config: updated lunarvim config for easier readability

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -4,6 +4,7 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+-- Add Nix parsing
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
 parser_config.nix = {
@@ -20,9 +21,34 @@ lvim.builtin.treesitter.ensure_installed = {
   "nix",
 }
 
+require("lvim.lsp.manager").setup("nil_ls", {
+  settings = {
+    nix = {
+      flake = {
+        autoArchive = true, -- removes prompt for flake inputs everytime you open *.nix
+        autoEvalInputs = true, -- should have more options
+      },
+    },
+  },
+})
+
+-- C language is broken, need to investigate
 lvim.builtin.treesitter.highlight.disable = { "c" }
 
+
+---
+-- Keybinds
+---
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
 
+lvim.keys.normal_mode["j"] = "h"
+lvim.keys.normal_mode["k"] = "j"
+lvim.keys.normal_mode["l"] = "k"
+lvim.keys.normal_mode[";"] = "l"
+
+lvim.keys.visual_mode["j"] = "h"
+lvim.keys.visual_mode["k"] = "j"
+lvim.keys.visual_mode["l"] = "k"
+lvim.keys.visual_mode[";"] = "l"
 
