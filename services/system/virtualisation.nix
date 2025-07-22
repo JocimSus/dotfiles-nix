34927e2bbebbaed69d5836e33f05ea4df68c9fdb
Jocim <joe.susatiyo@gmail.com>
2025-07-22 14:26:21 +0700

    virtualisation: moved specialisation to virtualisation.nix, added explicit vm trusted interface

--- services/system/virtualisation.nix
+++ services/system/virtualisation.nix
@@ -19,6 +19,29 @@
   #   ];
   # };
 
+  specialisation = {
+    vfio.configuration = {
+      system.nixos.tags = [ "vfio" ];
+      boot = {
+        kernelParams = [
+          "intel_iommu=on"
+          "vfio_pci.ids=10de:28a0,10de:22be" # NVIDIA
+          "video=efifb:off"
+          "video=vesafb:off"
+        ];
+        initrd.kernelModules = [
+          "vfio_pci"
+          "vfio"
+          "vfio_iommu_type1"
+        ];
+      };
+
+      services.xserver.videoDrivers = [ "intel" ];
+    };
+  };
+
+  networking.firewall.trustedInterfaces = [ "wlo1" "virbr0" ]; # as of 2025-07-22 need to explicitly allow libvirt NAT
+
   virtualisation = {
     spiceUSBRedirection.enable = true;
     libvirtd = {
