971bb40a57062f0286f5001c1098e2615d424d3c
jocim-server <>
2025-08-08 20:22:25 +0700

    system: updated formatting, added conservation mode (should limit battery)

--- system.nix
+++ system.nix
@@ -2,78 +2,80 @@
     pkgs,
     ...
 }: {
-    # Bootloader
-    boot = {
-        loader = {
-            efi = {
-                canTouchEfiVariables = true;
-                efiSysMountPoint = "/boot";
-            };
-            grub = {
-                enable = true;
-                efiSupport = true;
-                device = "nodev";
-            };
-        };
-
-        kernelPackages = pkgs.linuxPackages_zen;
-        kernelParams = [
-            "amdgpu.dc=1"
-            "amdgpu.dpm=1"
-            "amd.max_cstate=1"
-            "processor.max_cstate=1"
-            "idle=poll"
-	          "mem_sleep_default=deep"
-        ];
-    	extraModprobeConfig = ''
-          options snd_hda_intel power_save=0
-        '';
-    };
-
-    services.journald.extraConfig = ''
-        Storage=volatile
-	      SystemMaxUse=50M
-    '';
-
-    powerManagement.cpuFreqGovernor = "performance";
-
-    services.tlp = {
+  # Bootloader
+  boot = {
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
         enable = true;
-	      settings = {
-          TLP_DEFAULT_MODE = "BAT";
-          TLP_PERSISTENT_DEFAULT = 1; 
-        };
+        efiSupport = true;
+        device = "nodev";
+      };
     };
 
-    ## Networking ##
-    networking.resolvconf.enable = false;
-    networking.networkmanager.enable = true;
-    networking.nameservers = [ "94.140.14.14" ];
-    services.resolved = {
-      enable = true;
-      dnsovertls = "true";
-      fallbackDns = [ "94.140.15.15" "1.1.1.1" "1.0.0.1" ];
-    };
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelParams = [
+      "amdgpu.dc=1"
+      "amdgpu.dpm=1"
+      "amd.max_cstate=1"
+      "processor.max_cstate=1"
+      "idle=poll"
+      "mem_sleep_default=deep"
+    ];
+    extraModprobeConfig = ''
+      options snd_hda_intel power_save=0
+    '';
+  };
 
-    ## Timezone, locales ##
-    time.timeZone = "Asia/Jakarta";
-    i18n.defaultLocale = "en_US.UTF-8";
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-    };
+  services.journald.extraConfig = ''
+    Storage=volatile
+    SystemMaxUse=50M
+  '';
 
-    ## NixOS settings ##
-    nixpkgs.config.allowUnfree = true;
-    nix.settings = {
-        auto-optimise-store = true;
-        experimental-features = [ "nix-command" "flakes" ];
-    };
-    nix.gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 7d";
-    };
+  powerManagement.cpuFreqGovernor = "performance";
 
-    system.stateVersion = "24.11"; # Do not change
+  services.tlp = {
+    enable = true;
+    settings = {
+      TLP_DEFAULT_MODE = "BAT";
+      TLP_PERSISTENT_DEFAULT = 1;
+      START_CHARGE_THRESH_BAT0 = "0";
+      STOP_CHARGE_THRESH_BAT0  = "1";
+    };
+  };
+
+  ## Networking ##
+  networking.resolvconf.enable = false;
+  networking.networkmanager.enable = true;
+  networking.nameservers = [ "94.140.14.14" ];
+  services.resolved = {
+    enable = true;
+    dnsovertls = "true";
+    fallbackDns = [ "94.140.15.15" "1.1.1.1" "1.0.0.1" ];
+  };
+
+  ## Timezone, locales ##
+  time.timeZone = "Asia/Jakarta";
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  ## NixOS settings ##
+  nixpkgs.config.allowUnfree = true;
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+
+  system.stateVersion = "24.11"; # Do not change
 }
