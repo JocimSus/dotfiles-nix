ba8c456760aeb29d93c37bfac8381be70e669524
Jocim <joe.susatiyo@gmail.com>
2025-09-05 07:50:37 +0700

    services: added waydroid helper

--- services/system/waydroid.nix
+++ services/system/waydroid.nix
@@ -1,3 +1,17 @@
 {
+  pkgs,
+  ...
+}: {
   virtualisation.waydroid.enable = true;
+
+  environment.systemPackages = with pkgs; [ 
+    waydroid-helper 
+    nur.repos.ataraxiasjel.waydroid-script
+  ];
+
+  systemd = {
+    packages = [ pkgs.waydroid-helper ];
+    services.waydroid-mount.wantedBy = [ "multi-user.target" ];
+
+  };
 }
