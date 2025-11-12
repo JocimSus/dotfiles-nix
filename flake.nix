0b70f0a59fc4dab2f137dee5f9d58b4a4f4da579
jocim-server <>
2025-11-12 18:01:08 +0700

    flakes: changed to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "jocim's server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
