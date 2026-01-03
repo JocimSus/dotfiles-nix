8ea7caa7555404fa7c7c7e975fe8004ebcb1cfc7
jocim-nix <joe.susatiyo@gmail.com>
2026-01-03 19:59:49 +0700

    services: added sane for scanner

--- modules/services/printing/default.nix
+++ modules/services/printing/default.nix
@@ -5,4 +5,14 @@
     drivers = [ pkgs.hplip ];
   };
   services.ipp-usb.enable = true;
+
+	hardware.sane = {
+		enable = true;
+		extraBackends = [ pkgs.sane-backends ];
+	};
+	users.users.jocim-nix.extraGroups = [ "scanner" "lp" ];
+
+	environment.systemPackages = [
+		pkgs.naps2
+	];
 }
