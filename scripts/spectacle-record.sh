076a73276743dfc2d69719e1f20f938b4e7f462b
jocim-nix <joe.susatiyo@gmail.com>
2026-02-18 08:50:01 +0700

    scripts: added spectacle-record scripts * zipline now fixed

---
+++ scripts/spectacle-record.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+out=/tmp/video.mp4
+
+spectacle -R r -b -n -o "$out"
+ret=$?
+
+if [[ $ret -ne 0 ]] || [[ ! -s "$out" ]]; then
+  rm -f "$out"
+  exit 0
+fi
+
+curl \
+  -H "authorization: MTc2OTk0NTM0MDYxMw==.Zjc5ZTQ3ZmY2YjM5NmY3ZWRmZDYxYzg0LmU1Mzg2Nzk5MDYzMDJkNjA4MWI3OTBlZjkzZDVhOGE5MGE3MjNjYzQ1YTg3ZjEwNjM3NWIyN2U1ZTFhNmNkYzE1NWZmYzA3MjA1MjVkYjdmNGViNmFlZTA3ODJmZWQ5ZGU0NGE0ODFlNzVkNzExZDZhZTYyYjY4ODgwNzYxOWJlNTcuMWU2MjhkY2M2NGNjMGRiNWIyOGUwM2VjZjU2ZDFhYjc=" \
+  https://zip.224668.xyz/api/upload \
+  -F "file=@$out;type=video/mp4" \
+  -H 'content-type: multipart/form-data' \
+| jq -r .files[0].url \
+| tr -d '\n' \
+| wl-copy
+
+rm -f "$out"
