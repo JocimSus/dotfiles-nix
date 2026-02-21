6ed8084d3ac3dcbd3e8594939b009066993da96e
jocim-server <>
2026-02-21 22:11:16 +0700

    modules: refactored to a more flexible pattern, added max_body_size for zipline

--- modules/services/nginx/default.nix
+++ modules/services/nginx/default.nix
@@ -6,33 +6,52 @@
 let
   services = {
     # cloud = 8997; # let nextcloud manage itself
-    calibre = 8085; 
-    vault = 8222;
-    paperless = 28981;
-    zip = 8090;
-    note = 8017;
-    books = 8000;
-    yt = 5173;
-    yt-api = 3001;
-    dev-tokogo-api = 3334;
+    calibre = {
+      port = 8085;
+    }; 
+    vault = {
+      port = 8222;
+    };
+    # paperless = 28981;
+    zip = {
+      port = 8090;
+      nginx.extraConfig = ''
+        client_max_body_size 200M;
+      '';
+    };
+    note = {
+      port = 8017;
+    };
+    books = {
+      port = 8000;
+    };
+    yt = {
+      port = 5173;
+    };
+    yt-api = {
+      port = 3001;
+    };
+    dev-tokogo-api = {
+      port = 3334;
+    };
   };
 
-  local = lib.mapAttrs' (name: port: {
+  local = lib.mapAttrs' (name: opts: {
     name = "${name}.x.home";
     value = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString port}";
+        proxyPass = "http://127.0.0.1:${toString opts.port}";
       };
-    };
+    } // (opts.nginx or {});
   }) services;
 
-  public = lib.mapAttrs' (name: port: {
+  public = lib.mapAttrs' (name: opts: {
     name = "${name}.224668.xyz";
     value = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString port}";
+        proxyPass = "http://127.0.0.1:${toString opts.port}";
       };
-    };
+    } // (opts.nginx or {});
   }) services;
 
   manual = {
