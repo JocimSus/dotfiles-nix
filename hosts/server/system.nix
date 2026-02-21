e17c09a4e653ee95e28cb6d690aee451afba0a80
jocim-server <>
2026-02-21 12:56:37 +0700

    configuration: disabled dns

--- hosts/server/system.nix
+++ hosts/server/system.nix
@@ -63,16 +63,16 @@
   ## Networking ##
   networking.resolvconf.enable = false;
   networking.networkmanager.enable = true;
-  networking.nameservers = [ "94.140.14.14" ];
-  services.resolved = {
-    enable = true;
-    dnsovertls = "true";
-    fallbackDns = [
-      "94.140.15.15"
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
-  };
+  # networking.nameservers = [ "94.140.14.14" ];
+  # services.resolved = {
+  #   enable = true;
+  #   dnsovertls = "true";
+  #   fallbackDns = [
+  #     "94.140.15.15"
+  #     "1.1.1.1"
+  #     "1.0.0.1"
+  #   ];
+  # };
 
   ## Timezone, locales ##
   time.timeZone = "Asia/Jakarta";
