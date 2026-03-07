589692bce1e278da7140eba205ab8cab73c3bfcb
jocim-nix <joe.susatiyo@gmail.com>
2026-03-07 08:04:06 +0700

    hm: formatted, added gradle, added jdtls, neotest/-jdtls

--- hosts/msi-laptop/home.nix
+++ hosts/msi-laptop/home.nix
@@ -23,156 +23,184 @@
     categories = [ "Game" ];
   };
 
-  programs = {
-    home-manager.enable = true;
-    kitty = {
-      enable = true;
-      extraConfig = lib.readFile .config/kitty/kitty.conf;
-    };
-    tmux = {
-      enable = true;
-      keyMode = "vi";
-      mouse = true;
-      baseIndex = 1;
-      escapeTime = 10;
-      focusEvents = true;
-      terminal = "screen-256color";
-      tmuxp.enable = true;
-    };
-    zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-      initContent = lib.mkAfter ''
-        				bindkey "^[[1;5D" backward-word
-        				bindkey "^[[1;5C" forward-word
-
-        				export EDITOR="nvim"
-      '';
+  programs =
+    let
       shellAliases = {
         # ssh = "ssh -i ${config.home.homeDirectory}/.ssh/meow";
         c = "clear";
         cdd = "cd ~/.dotfiles";
         cdc = "cd ~/College";
       };
-    };
-    oh-my-posh = {
-      enable = true;
-      enableZshIntegration = true;
-      # because i needed to use --config on omp, was forced to do it like this
-      settings = builtins.fromTOML (builtins.readFile .config/ohmyposh/jocims.omp.toml);
-      # useTheme = "easy-term"; # https://ohmyposh.dev/docs/themes
-    };
-    gradle.enable = true;
-    neovim =
-      let
-        fromFile = f: "${builtins.readFile f}";
-        treesitter-parsers = pkgs.symlinkJoin {
-          name = "treesitter-parsers";
-          paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
-        };
-      in
-      {
+    in
+    {
+      home-manager.enable = true;
+      kitty = {
         enable = true;
-
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
-
-        initLua = ''
-          ${builtins.readFile .config/nvim/options.lua}
-          ${builtins.readFile .config/nvim/plugins/treesitter.lua}
-        '';
-
-        extraPackages = with pkgs; [
-          xclip
-          wl-clipboard
-
-          gcc
-          file
-          lua-language-server
-          clang-tools
-          pyright
-          nixd
-          nil
-          jdt-language-server
-          gradle
-
-          nixfmt
-        ];
-
-        plugins = with pkgs.vimPlugins; [
-          mini-icons
-          nvim-web-devicons
-          luasnip
-          friendly-snippets
-          cmp-nvim-lsp
-          cmp_luasnip
-          nvim-treesitter.withAllGrammars
-          nvim-dap
-          nvim-dap-python
-          nvim-dap-virtual-text
-          nvim-nio
-          plenary-nvim
-          nvim-jdtls
-          {
-            plugin = neodev-nvim;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/neodev.lua;
-          }
-          {
-            plugin = nvim-lspconfig;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/lsp.lua;
-          }
-          {
-            plugin = comment-nvim;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/comments.lua;
-          }
-          {
-            plugin = catppuccin-nvim;
-            config = "colorscheme catppuccin-mocha";
-          }
-          {
-            plugin = lualine-nvim;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/lualine.lua;
-          }
-          {
-            plugin = nvim-cmp;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/cmp.lua;
-          }
-          {
-            plugin = telescope-fzf-native-nvim;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/telescope.lua;
-          }
-          {
-            plugin = nvim-autopairs;
-            type = "lua";
-            config = "require('nvim-autopairs').setup()";
-          }
-          {
-            plugin = nvim-dap;
-            type = "lua";
-            config = fromFile .config/nvim/plugins/dap.lua;
-          }
-          {
-            plugin = nvim-dap-ui;
-            type = "lua";
-            config = "require('dapui').setup()";
-          }
-          {
-            plugin = render-markdown-nvim;
-            type = "lua";
-            config = "require('render-markdown').setup({})";
-          }
-        ];
+        extraConfig = lib.readFile .config/kitty/kitty.conf;
       };
-  };
+      tmux = {
+        enable = true;
+        keyMode = "vi";
+        mouse = true;
+        baseIndex = 1;
+        escapeTime = 10;
+        focusEvents = true;
+        terminal = "screen-256color";
+        tmuxp.enable = true;
+      };
+      zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+        initContent = lib.mkAfter ''
+          				bindkey "^[[1;5D" backward-word
+          				bindkey "^[[1;5C" forward-word
+
+          				export EDITOR="nvim"
+        '';
+        shellAliases = shellAliases;
+      };
+      bash = {
+        enable = true;
+        enableCompletion = true;
+        shellAliases = shellAliases;
+      };
+      oh-my-posh = {
+        enable = true;
+        enableZshIntegration = true;
+        # because i needed to use --config on omp, was forced to do it like this
+        settings = builtins.fromTOML (builtins.readFile .config/ohmyposh/jocims.omp.toml);
+        # useTheme = "easy-term"; # https://ohmyposh.dev/docs/themes
+      };
+      gradle = {
+        enable = true;
+        package = pkgs.gradle_9;
+      };
+      neovim =
+        let
+          fromFile = f: "${builtins.readFile f}";
+          treesitter-parsers = pkgs.symlinkJoin {
+            name = "treesitter-parsers";
+            paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
+          };
+        in
+        {
+          enable = true;
+
+          viAlias = true;
+          vimAlias = true;
+          vimdiffAlias = true;
+
+          initLua = ''
+            ${builtins.readFile .config/nvim/options.lua}
+            ${builtins.readFile .config/nvim/plugins/treesitter.lua}
+          '';
+
+          extraPackages = with pkgs; [
+            xclip
+            wl-clipboard
+
+            gcc
+            file
+            lua-language-server
+            clang-tools
+            pyright
+            nixd
+            nil
+            jdt-language-server
+            gradle
+
+            nixfmt
+          ];
+
+          plugins = with pkgs.vimPlugins; [
+            mini-icons
+            nvim-web-devicons
+            luasnip
+            friendly-snippets
+            cmp-nvim-lsp
+            cmp_luasnip
+            nvim-treesitter.withAllGrammars
+            nvim-dap
+            nvim-dap-python
+            nvim-dap-virtual-text
+            nvim-nio
+            plenary-nvim
+            nvim-jdtls
+            neotest-java
+            {
+              plugin = neodev-nvim;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/neodev.lua;
+            }
+            {
+              plugin = nvim-lspconfig;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/lsp.lua;
+            }
+            {
+              plugin = comment-nvim;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/comments.lua;
+            }
+            {
+              plugin = catppuccin-nvim;
+              config = "colorscheme catppuccin-mocha";
+            }
+            {
+              plugin = lualine-nvim;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/lualine.lua;
+            }
+            {
+              plugin = nvim-cmp;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/cmp.lua;
+            }
+            {
+              plugin = telescope-fzf-native-nvim;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/telescope.lua;
+            }
+            {
+              plugin = nvim-autopairs;
+              type = "lua";
+              config = "require('nvim-autopairs').setup()";
+            }
+            {
+              plugin = nvim-dap;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/dap.lua;
+            }
+            {
+              plugin = nvim-dap-ui;
+              type = "lua";
+              config = "require('dapui').setup()";
+            }
+            {
+              plugin = render-markdown-nvim;
+              type = "lua";
+              config = "require('render-markdown').setup({})";
+            }
+            {
+              plugin = mini-icons;
+              type = "lua";
+              config = "require('mini.icons').setup()";
+            }
+            {
+              plugin = which-key-nvim;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/which-key.lua;
+            }
+            {
+              plugin = neotest;
+              type = "lua";
+              config = fromFile .config/nvim/plugins/neotest.lua;
+            }
+          ];
+        };
+    };
 
   ## Home ##
   home = {
