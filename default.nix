ece7be1fe79b3898bd2adf729f4e2973416e4cdc
jocim-nix <joe.susatiyo@gmail.com>
2026-03-25 17:52:31 +0700

    overlays: flattenned my pkgs

--- default.nix
+++ default.nix
@@ -4,10 +4,13 @@
 let
   names = builtins.attrNames (builtins.readDir ./pkgs);
   filtered = builtins.filter (n: n != "default.nix") names;
+
+  load =
+    name:
+    let
+      v = pkgs.callPackage (./pkgs + "/${name}") { };
+    in
+    if builtins.isAttrs v && !(v ? type && v.type == "derivation") then v else { ${name} = v; };
+
 in
-builtins.listToAttrs (
-  map (name: {
-    inherit name;
-    value = pkgs.callPackage (./pkgs + "/${name}") { };
-  }) filtered
-)
+builtins.foldl' (acc: name: acc // (load name)) { } filtered
