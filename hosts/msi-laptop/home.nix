cbad831d6e104ad0f6c06e5973e4e43f30fa8009
JocimSus <joe.susatiyo@gmail.com>
2026-07-12 22:37:21 +0700

    hosts(msi-laptop): riced tmux, modified keybindings for vim like feel

--- hosts/msi-laptop/home.nix
+++ hosts/msi-laptop/home.nix
@@ -34,6 +34,33 @@
         focusEvents = true;
         terminal = "screen-256color";
         tmuxp.enable = true;
+
+        extraConfig = ''
+          # Vim like keybindings
+          bind h split-window -v
+          bind v split-window -h
+
+          bind -n C-h select-pane -L
+          bind -n C-j select-pane -D
+          bind -n C-k select-pane -U
+          bind -n C-l select-pane -R
+
+          # Kitty config
+          set -as terminal-features ",xterm-kitty:24bit"
+
+          # Colors from kitty.conf
+          # Catppuccin Mocha Styling
+          set -g status-style bg="#181825",fg="#cdd6f4"
+
+          setw -g window-status-current-style bg="#cba6f7",fg="#11111b",bold
+          setw -g window-status-current-format " #I:#W "
+
+          setw -g window-status-style bg="#181825",fg="#585b70"
+          setw -g window-status-format " #I:#W "
+
+          set -g pane-border-style fg="#6c7086"       
+          set -g pane-active-border-style fg="#b4befe"
+        '';
       };
       zsh = {
         enable = true;
