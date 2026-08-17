b9c802b4b9fc063ec33f67a63debac6bc8e2cb24
JocimSus <joe.susatiyo@gmail.com>
2026-08-17 16:35:51 +0700

    modules(minio): temporarily allowed unsafe minio package

--- modules/services/minio/default.nix
+++ modules/services/minio/default.nix
@@ -72,6 +72,18 @@ in
       rootCredentialsFile = config.sops.secrets.${cfg.sops.rootCredentialsFile}.path;
     };
 
+    # Known issues:
+    # - CVE-2026-40344: MinIO has an Unauthenticated Object Write via Missing Signature Verification in Unsigned-Trailer Uploads
+    # - CVE-2026-41145: Unauthenticated Object Write via Query-String Credential Signature Bypass in Unsigned-Trailer Uploads
+    # - CVE-2026-33322: JWT Algorithm Confusion in OIDC Authentication
+    # - CVE-2026-33419: LDAP login brute-force via user enumeration and missing rate limit
+    # - CVE-2026-34204: SSE Metadata Injection via Replication Headers
+    # - CVE-2026-39414: DoS via Unbounded Memory Allocation in S3 Select CSV Parsing
+    # - minio has been abandoned by upstream and security issues won't be fixed. Users should migrate to alternatives such as Garage, SeaweedFS, or Ceph. S3-compatible clients such as rclone can be used to move data.
+    nixpkgs.config.permittedInsecurePackages = [
+      "minio-2025-04-22T22-12-26Z"
+    ];
+
     systemd.services.minio.environment = {
       MINIO_SERVER_URL = "https://${cfg.listenDomain}";
       MINIO_BROWSER_REDIRECT_URL = "https://${cfg.consoleDomain}";
