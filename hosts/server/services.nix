74f560db7afd54ff6b39b5c545a203085a2008ed
jocim-server <>
2026-08-17 15:00:49 +0700

    hosts(server): added fisiomate api endpoint

--- hosts/server/services.nix
+++ hosts/server/services.nix
@@ -33,6 +33,17 @@
   woof.network.basePublicDomain = "jocimsus.tech";
   woof.network.baseLocalDomain = "home";
 
+  services.nginx.virtualHosts."fisiomate-api.jocimsus.tech".locations."/" = {
+    proxyPass = "http://127.0.0.1:8857";
+    proxyWebsockets = true;
+    extraConfig = ''
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+    '';
+  };
+
   woof = {
     #
     # Apps
