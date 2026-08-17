f222c3400946a6eacaea970161cd186523a5bca9
JocimSus <joe.susatiyo@gmail.com>
2026-08-17 16:05:57 +0700

    flakes: bump nixpkgs-stable to 26.05 because of unsafe docker version for woof

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-25_05.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
