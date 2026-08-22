6b94a0d3e964dfd63ef3f8609803c34731eed36a
JocimSus <joe.susatiyo@gmail.com>
2026-08-22 21:30:51 +0700

    hosts(msi-laptop): disabled hyprland (genuinely forgot)

--- hosts/msi-laptop/system.nix
+++ hosts/msi-laptop/system.nix
@@ -73,7 +73,6 @@
   };
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  programs.hyprland.enable = true;
 
   ## Graphics ##
   hardware.graphics = {
